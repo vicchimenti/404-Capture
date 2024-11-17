31234990186897c8f5599eb5229dc3a3b4e1cfe5
Vic Chimenti <chimenti@seattleu.edu>
2024-11-17 05:56:06 -0800

    import from site 2023

---
+++ capture-referrer.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', function() {
+ 
+
+
+    // get referrer
+    function getReferrer () {
+
+        return document.referrer.toString();
+    }
+
+    // get hostname
+    function getHostname () {
+
+        return document.referrer;
+    }
+
+    // get current page
+    function getCurrentPage () {
+
+        return window.location.href.toString();
+    }
+
+
+
+    // Main
+    try {
+
+    
+        // content dictionary
+        let info = {
+
+            origin: getReferrer(),
+            current: getCurrentPage(),
+            hostname: getHostname(),
+      
+        };
+
+        // console.log("hostname: " + info.hostname.split('/')[2]);
+        // console.log("referrer: " + info.origin);
+        // console.log("current: " + info.current);
+        let hostString = info.hostname.split('/')[2];
+
+
+
+        function autoFillAndSubmit() {
+
+            // Fill in form fields
+            document.getElementById('referrer-url').value = info.origin;
+            document.getElementById('referrer-hostname').value = hostString;
+            document.getElementById('current-page').value = info.current;
+            document.getElementsByClassName('pull-right')[0].click();
+   
+        }
+
+
+
+        // Call the function when needed, e.g., on page load
+        window.onload = setTimeout(() => {
+            autoFillAndSubmit();
+            // console.log("Delayed for 1.5 seconds");
+        }, 2000);
+
+
+
+
+        
+
+    } catch (err) {
+        document.write(err);
+    }
+});
